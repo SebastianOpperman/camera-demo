5652e24afffbd0a8c51596d063e33fcb1c570a56
Sebastian <oppermansebastian@gmail.com>
2021-03-02 13:44:03 +0200

    change

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import React, {useRef, useEffect, useState} from 'react'
+
 export default function Home() {
 
+	const [stream, setStream] = useState(null)
+	const player = useRef(null)
+
 	async function stream() {
-		const stream = await navigator.mediaDevices.getUserMedia({
+		setStream(await navigator.mediaDevices.getUserMedia({
 			video: {
 				minAspectRatio: 1.333,
 				minFrameRate: 60,
@@ -9,12 +14,17 @@ export default function Home() {
 				heigth: 480
 			},
 			audio: true
-		})
+		}))
 	}
+
+	useEffect(() => {
+		player.src = stream
+	}, [player, stream])
 
 	return (
 		<main>
 			<button onClick={() => stream()}>Start Stream</button>
+			<video autoplay ref={player}></video>
 		</main>
 	)
 }
