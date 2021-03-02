3af8b6f2f2ace14ad6d58e4707f9619332040206
Sebastian <oppermansebastian@gmail.com>
2021-03-02 13:10:37 +0200

    Updates

---
+++ _next/static/8cxQjkwquWFYS0FQjtmlZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-f83bdd3ace723d4ea28b.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f5193b40c15e92215afe.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
